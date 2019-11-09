f923ae9d386cfeda83335ba43a0de07ea68c657b
Bugsy <bugsyborromeo@hotmail.com>
2019-11-09 16:16:01 +1100

    Revert "attempt 1 at radar triangle"
    This reverts commit 0f27a5e3d9dc86bfb29f675e8785f2a3c4195809.

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -47,46 +47,51 @@ class CarInterface(CarInterfaceBase):
 
     ret.enableCruise = True
     ret.steerLimitAlert = True
-    ret.openpilotLongitudinalControl = False
 
     # force openpilot to fake the stock camera, since car harness is not supported yet and old style giraffe (with switches)
     # was never released
     ret.enableCamera = True
 
     ret.steerRateCost = 0.7
-    ret.steerMaxBP = [0.] # m/s
-    ret.steerMaxV = [0.0294]
-    ret.centerToFront = ret.wheelbase * 0.5
-    ret.transmissionType = car.CarParams.TransmissionType.automatic
 
     if candidate in [CAR.IMPREZA]:
       ret.mass = 1568. + STD_CARGO_KG
       ret.wheelbase = 2.67
+      ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 15
       ret.steerActuatorDelay = 0.4   # end-to-end angle controller
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
+      ret.steerMaxBP = [0.] # m/s
+      ret.steerMaxV = [1.]
 
     if candidate in [CAR.OUTBACK]:
       ret.mass = 1568 + STD_CARGO_KG
       ret.wheelbase = 2.67
-      ret.steerRatio = 14
+      ret.centerToFront = ret.wheelbase * 0.5
+      ret.steerRatio = 20           # learned, 14 stock
       tire_stiffness_factor = 1.0
-      ret.steerActuatorDelay = 0.2
+      ret.steerActuatorDelay = 0.07
+      ret.steerRateCost = 0.7
       ret.lateralTuning.pid.kf = 0.000043
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 10., 20.], [0., 10., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.008, 0.05, 0.2], [0.003, 0.018, 0.025]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.05, 0.2], [0.003, 0.018, 0.025]]
+      ret.steerMaxBP = [0.] # m/s
+      ret.steerMaxV = [1.]
 
     if candidate in [CAR.LEGACY]:
       ret.mass = 1568 + STD_CARGO_KG
       ret.wheelbase = 2.67
-      ret.steerRatio = 14.5
+      ret.centerToFront = ret.wheelbase * 0.5
+      ret.steerRatio = 12.5   #14.5 stock
       tire_stiffness_factor = 1.0
       ret.steerActuatorDelay = 0.15
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1, 0.2], [0.01, 0.02]]
+      ret.steerMaxBP = [0.] # m/s
+      ret.steerMaxV = [1.]
 
     ret.steerControlType = car.CarParams.SteerControlType.torque
     ret.steerRatioRear = 0.

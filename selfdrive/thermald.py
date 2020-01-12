689d3a64bd040191de5da928a014946632e5fa6c
John Murphy <jmurphy73@gmail.com>
2020-01-12 13:49:20 -0500

    Modified fan speed values for Noctua fan.

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -94,13 +94,13 @@ def set_eon_fan(val):
     last_eon_fan_val = val
 
 # temp thresholds to control fan speed - high hysteresis
-_TEMP_THRS_H = [50., 65., 80., 10000]
+_TEMP_THRS_H = [30., 65., 80., 10000]
 # temp thresholds to control fan speed - low hysteresis
-_TEMP_THRS_L = [42.5, 57.5, 72.5, 10000]
+_TEMP_THRS_L = [22.5, 57.5, 72.5, 10000]
 # fan speed options
-_FAN_SPEEDS = [0, 16384, 32768, 65535]
+_FAN_SPEEDS = [0, 65535, 65535, 65535]
 # max fan speed only allowed if battery is hot
-_BAT_TEMP_THERSHOLD = 45.
+_BAT_TEMP_THERSHOLD = 25.
 
 
 def handle_fan_eon(max_cpu_temp, bat_temp, fan_speed):
